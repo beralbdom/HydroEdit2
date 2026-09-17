5318fcd8be0da6cf7a506c598ceb46c2d27f73c9
Bernardo <beralbdom@gmail.com>
2026-09-17 03:18:33 -0300

    Linhas alternadas em cinza derivado da cor base

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,9 +1,19 @@
 #include <QApplication>
+#include <QPalette>
 #include "janela_principal.h"
+
+// O Fusion no modo escuro do Windows usa azul nas linhas alternadas; troca por um cinza proximo do fundo.
+static void ajustarLinhasAlternadas(QApplication& app) {
+    QPalette pal = app.palette();
+    QColor base = pal.color(QPalette::Base);
+    pal.setColor(QPalette::AlternateBase, base.lightness() < 128 ? base.lighter(130) : base.darker(104));
+    app.setPalette(pal);
+}
 
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
     QApplication::setStyle(QStringLiteral("Fusion"));
+    ajustarLinhasAlternadas(app);
     QApplication::setOrganizationName(QStringLiteral("HydroEdit"));
     QApplication::setApplicationName(QStringLiteral("HydroEdit"));
     QApplication::setApplicationVersion(QStringLiteral("5.0.0"));
